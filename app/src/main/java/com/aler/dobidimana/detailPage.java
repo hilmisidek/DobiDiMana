ee52b53f80e12c3ba60f8e687987a31df40aa09d
hilmisidek <zalikamari@gmail.com>
2018-10-11 09:30:21 +0800

    vcs integrated

--- app/src/main/java/com/aler/dobidimana/detailPage.java
+++ app/src/main/java/com/aler/dobidimana/detailPage.java
@@ -64,7 +64,7 @@ double longFinal;
         TextView addressText=(TextView)findViewById(R.id.addree_Text);
         nameText.setText(nameDetail);
         addressText.setText(addressDetail+","+latitudeDetail+","+longitudeDetail+"\n Distance:"+distanceFinal+" "+unit);
-//VCS DDD
+
 
 //        https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=YOUR_API_KEY
 
